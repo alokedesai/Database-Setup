14050615c428755d2ba7ca7fff48c0780014ceab
Aloke Desai <aloke.desai@gmail.com>
2013-07-31 20:59:30 -0400

    added search and improved form functionality

--- app.py
+++ app.py
@@ -16,18 +16,36 @@ db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+curID = None
 def str(x):
     return unicodedata.normalize('NFKD',x).encode('ascii','ignore')
 
-
+def uniquifyo(xl):
+    final = []
+    ranked = {}
+    for x in xl:
+        if x in ranked:
+            ranked[x] += 1
+            continue
+        ranked[x] = 1
+    sorted_x = sorted(ranked.iteritems(), key=operator.itemgetter(1),reverse=True)
+    for y in sorted_x:
+        final.append(y[0])
+    return final
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
-    name = db.Column(db.String(120), unique = False)
+    first_name = db.Column(db.String(120), unique = False)
+    last_name = db.Column(db.String(120), unique = False)
     school = db.Column(db.String(120), unique = False)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(12), unique=False)
+    password_description = db.Column(db.String(200), unique=False)
+    grad_year = db.Column(db.String(4), unique=False)
+    major = db.Column(db.String(120), unique=False)
+    role = db.Column(db.String(80), unique=False)
     skills = db.relationship('Skills',backref='user',lazy='dynamic')
+    
     
     def is_authenticated(self):
         return True
@@ -39,14 +57,22 @@ class User(db.Model):
 
     def get_id(self):
         return unicode(self.id)
+    
 
-
-    def __init__(self, username, email, password, name, school):
+    def __init__(self, username, first_name, last_name, email, password, password_description, school, grad_year, major, role):
         self.username = username
         self.email = email
         self.password = password
-        self.name = name
         self.school = school
+        self.role = role
+        self.first = first_name
+        self.last = last_name
+        self.password_description = password_description
+        self.grad_year = grad_year
+        self.major = major
+
+    def get_role(self):
+        return self.role
 
     def __repr__(self):
         return "<User %r>" % self.username
@@ -80,7 +106,16 @@ class Skills(db.Model):
 
     def __repr__(self):
         return "<Skill %r>" % self.skill
+class Experience(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    company = db.Column(db.String(80))
+    start_date = db.Column(db.String(20))
+    end_date = db.Column(db.String(20))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+    def __repr__(self):
+        return "<Experience %r>" % self.title
 
 @login_manager.user_loader
 def load_user(userid):
@@ -91,65 +126,156 @@ def index():
     # return str(User.query.all()[0].password) # I don't think this will 
     
     return render_template("index.html")
+
 @app.route("/signup")
 def signup():
-    return render_template("new.html")
+    return render_template("signup.html")
 
-@app.route("/results", methods=["POST"])
-def results():
-    error = None
-    theUser = request.form["username"]
-    email = request.form["email"]
-    skill = request.form.getlist('skills')
-    for users in User.query.all():
-        if theUser == users.username:
-            error = "already a username"
+@app.route("/signup/college",  methods=["POST", "GET"])
+def collegesignup():
+    if request.method == "POST":
+        error = None
+        theUser = request.form["username"].lower()
+        email = request.form["email"].lower()
+        skill = request.form.getlist('skills')
+        first_name = request.form["firstName"]
+        last_name = request.form["lastName"]
+        school = request.form["school"]
+        year = request.form["year"]
+        major = request.form["major"]
+        password = request.form["password"]
+        description = request.form["description"]
 
-        if email == users.email:
-            error= "already a registered email"
+        
+        c1 = request.form["company1"]
+        c2 = request.form["company2"]
+        c3 = request.form["company3"]
 
-    try:
-        x = User(theUser, email, request.form["password"], request.form["name"], request.form["school"])
-        db.session.add(x)
-        try :
-            db.session.commit()
-            for s in skill:
-                S = Skills(skill=s,user=x)
-                db.session.add(S)
-            try:
+        t1 = request.form["title1"]
+        t2 = request.form["title2"]
+        t3 = request.form["title3"]
+
+
+        s1 = request.form["startdate1"]
+        s2 = request.form["startdate2"]
+        s3 = request.form["startdate3"]
+
+        e1 = request.form["enddate1"]
+        e2 = request.form["enddate2"]
+        e3 = request.form["enddate3"]
+
+      
+        for users in User.query.all():
+            if theUser == users.username:
+                error = "already a username"
+                return render_template("new.html", error= error)
+            if email == users.email:
+                error= "already a registered email"
+                return render_template("new.html", error= error)
+
+        for item in [theUser, first_name, last_name, email, school, year, major, password, c1, t1,s1,e1]:
+            if item == None or item == "":
+                return render_template("new.html", error = "You failed to fill in a required field" + str(item))
+        try:
+            x = User(theUser, first_name, last_name, email, password,description, school, year, major, "developer")
+            db.session.add(x)
+            try :
                 db.session.commit()
+                for s in skill:
+                    S = Skills(skill=s,user=x)
+                    db.session.add(S)
+                
+                try:
+                    db.session.commit()
+
+                    # for i in range(1,4):
+                    #     if t[i] != "":
+                    #         E = Experience(title = t[i], company = c[i], start = s[i], end = e[i], user=x)
+                    #         db.session.add(E)
+                    # try:
+                    #     db.session.commit()
+                    # except:
+                    #     db.session.rollback()
+                    #     return "couldn't commit experience"
+                except Exception as e:
+                    db.session.rollback()
+                    return "couldn't commit skills or experience"
             except Exception as e:
                 db.session.rollback()
-                return "broken"
-        except Exception as e:
-            db.session.rollback()
-            return "broken"
+                return "couldn't commit main"
+                
+            
+        except IntegrityError:
+            print "Not Working"
+        string = unicodedata.normalize('NFKD',x.username).encode('ascii','ignore')
+       
+        return redirect("/signin")
+
+    return render_template("new.html")
+
+# @app.route("/results", methods=["POST"])
+# def results():
+#     error = None
+#     theUser = request.form["username"].lower()
+#     email = request.form["email"].lower()
+#     skill = request.form.getlist('skills')
+#     for users in User.query.all():
+#         if theUser == users.username:
+#             error = "already a username"
+
+#         if email == users.email:
+#             error= "already a registered email"
+
+#     try:
+#         x = User(theUser, email, request.form["password"].lower(), request.form["name"], request.form["school"], request.form["type"])
+#         db.session.add(x)
+#         try :
+#             db.session.commit()
+#             for s in skill:
+#                 S = Skills(skill=s,user=x)
+#                 db.session.add(S)
+#             try:
+#                 db.session.commit()
+#             except Exception as e:
+#                 db.session.rollback()
+#                 return "broken"
+#         except Exception as e:
+#             db.session.rollback()
+#             return "broken"
             
         
-    except IntegrityError:
-        print "Not Working"
-    string = unicodedata.normalize('NFKD',x.username).encode('ascii','ignore')
+#     except IntegrityError:
+#         print "Not Working"
+#     string = unicodedata.normalize('NFKD',x.username).encode('ascii','ignore')
    
-    return string #render_template("results.html", user=User.query.all(), error = error)
+#     return string #render_template("results.html", user=User.query.all(), error = error)
 
-@app.route('/signin', methods = ["GET"])
+@app.route('/signin', methods = ["GET", "POST"])
 def signin():
+    if request.method == "POST":
+        error = None
+        form = LoginForm()
+        theUser = request.form["username"].lower()
+        password = request.form["password"].lower()
+        print theUser
+        for users in User.query.all():
+            error = None
+            if theUser == users.email:
+
+                if password == users.password:
+                    curID = users.id
+                    user = load_user(users.id)
+                    login_user(user)
+                    flash('Logged ' + theUser + ' in successfully.')
+                    return redirect('/settings/'+ users.username)
+                else: 
+                    error = "Invalid username and password combination." 
+                    return render_template("signin.html", error = error)     
+        error = "Invalid username and password combination."
+        return render_template("signin.html", error = error) 
     return render_template("signin.html")
 
-@app.route('/login', methods = ["GET","POST"])
-def login():
-    form = LoginForm()
-    theUser = request.form["username"]
-    password = request.form["password"]
-    for users in User.query.all():
-        if theUser == users.username:
-            if password == users.password:
-                user = load_user(users.id)
-                login_user(user)
-                flash('Logged ' + theUser + ' in successfully.')
-                return redirect('/settings/'+theUser)
-            else: return "Invalid Password!"        
-    return "Invalid Username!!!"
+
 @app.route("/logout")
 @login_required
 def logout():
@@ -160,20 +286,36 @@ def logout():
 @app.route("/settings/<username>")
 @login_required
 def settings(username):
-    cS = []
-    cR = []
-    s = []
-    user = User.query.filter_by(username=username).first()
-    skill = user.skills.all()
-    conversationsS = Conversation.query.filter_by(user1=unicodedata.normalize('NFKD',user.username).encode('ascii','ignore')).all()
-    conversationsR = Conversation.query.filter_by(user2=unicodedata.normalize('NFKD',user.username).encode('ascii','ignore')).all()
-    for c in conversationsS:
-        cS.append({'user': str(c.user2), 'subject': str(c.subject), 'timestamp': c.timestamp, 'id': c.id})
-    for c in conversationsR:
-        cR.append({'user': str(c.user1), 'subject': str(c.subject), 'timestamp': c.timestamp, 'id': c.id})
-    for SKILLS in skill:
-        s.append(unicodedata.normalize('NFKD',SKILLS.skill).encode('ascii','ignore'))
-    return render_template("settings.html",username=username,s=s,cS=cS,cR=cR)
+    if User.query.get(current_user.get_id()).role == "developer":
+        cS = []
+        cR = []
+        s = []
+        user = User.query.filter_by(username=username).first()
+        skill = user.skills.all()
+        conversationsS = Conversation.query.filter_by(user1=unicodedata.normalize('NFKD',user.username).encode('ascii','ignore')).all()
+        conversationsR = Conversation.query.filter_by(user2=unicodedata.normalize('NFKD',user.username).encode('ascii','ignore')).all()
+        for c in conversationsS:
+            cS.append({'user': str(c.user2), 'subject': str(c.subject), 'timestamp': c.timestamp, 'id': c.id})
+        for c in conversationsR:
+            cR.append({'user': str(c.user1), 'subject': str(c.subject), 'timestamp': c.timestamp, 'id': c.id})
+        for SKILLS in skill:
+            s.append(unicodedata.normalize('NFKD',SKILLS.skill).encode('ascii','ignore'))
+        return render_template("settings.html",username=username,s=s,cS=cS,cR=cR)
+    else:
+        cS = []
+        cR = []
+        s = []
+        user = User.query.filter_by(username=username).first()
+
+        conversationsS = Conversation.query.filter_by(user1=unicodedata.normalize('NFKD',user.username).encode('ascii','ignore')).all()
+        conversationsR = Conversation.query.filter_by(user2=unicodedata.normalize('NFKD',user.username).encode('ascii','ignore')).all()
+        for c in conversationsS:
+            cS.append({'user': str(c.user2), 'subject': str(c.subject), 'timestamp': c.timestamp, 'id': c.id})
+        for c in conversationsR:
+            cR.append({'user': str(c.user1), 'subject': str(c.subject), 'timestamp': c.timestamp, 'id': c.id})
+        user = User.query.filter_by(username=username).first()
+
+        return render_template("company-settings.html",username=username,s=s,cS=cS,cR=cR)
 
 @app.route("/conversation/<ID>/<user>")
 @login_required
@@ -212,7 +354,36 @@ def replying():
             return "brokenm"
     return redirect('settings/'+Sender)
         
+@app.route("/search",methods=["GET","POST"])
+def search():
+    schools = []
+    users = User.query.all()
+    for user in users:
+        if str(user.school) == "" : continue
+        schools.append(str(user.school))
+    schools = list(set(schools))
+    return render_template('search.html',schools=schools)
 
+@app.route("/sresults",methods=["GET","POST"])
+def sresults():
+    schools= request.form.getlist("schools")
+    skills = request.form.getlist('skills')
+    users = []
+    
+    for sch in schools:
+        users += User.query.filter_by(school=sch).all()
+    for sk in skills:
+        skillList = Skills.query.filter_by(skill=sk).all()
+        for s in skillList:
+            users.append(s.user)
+
+    users = uniquifyo(users)
+    names = []
+    for user in users:
+        names.append(str(user.username))
+    return render_template('sresults.html',names=names)
+
+    
 @app.route("/start",methods=["GET","POST"])
 @login_required
 def start():
@@ -244,6 +415,23 @@ def start():
 @login_required
 def converse(username):
     return render_template("compose.html",username=username)
+@app.route("/profile/<username>")
+@login_required 
+def profile(username):
+
+    if User.query.get(current_user.get_id()).role == "developer":
+
+        cS = []
+        cR = []
+        s = []
+        user = User.query.filter_by(username=username).first()
+        skill = user.skills.all()
+       
+        for SKILLS in skill:
+            s.append(unicodedata.normalize('NFKD',SKILLS.skill).encode('ascii','ignore'))
+        return render_template("profile.html",user = user, username=username,s=s)
+    else:
+        return "not setup yet"
 
 
 if __name__ == "__main__":

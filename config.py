b0b11f6d9dac0bd7ecad5bd470b9d35057d0b9c5
usuallyacorn <rllluo@gmail.com>
2013-08-03 11:41:38 -0700

    Database configuratoin

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ SECRET_KEY = 'leelelelelele'
 UPLOAD_FOLDER= os.path.realpath('.') + '/app/static/'
 ALLOWED_EXTENSIONS=set(['txt','pdf','doc','odt','docx'])
 
-SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir,'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
 DEFAULT_FILE_STORAGE = 'filesystem'
 MAX_CONTENT_LENGTH = 16 * 1024 * 1024

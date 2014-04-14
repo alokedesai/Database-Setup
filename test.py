fa1f9571bfdb792941a13dc0af983716524a0091
Aloke Desai <aloke.desai@gmail.com>
2014-04-13 22:07:10 -0700

    remove everything

--- test.py
+++
@@ -1,8 +0,0 @@
-from app import models,db
-
-users = models.User.query.all()
-a = models.Conversation(user=users[0],user2=users[1],extra_data="hi")
-db.session.rollback()
-db.session.add(a)
-db.session.commit()
-
